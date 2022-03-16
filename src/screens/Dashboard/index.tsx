f79435e0f2693fa314846f7c64509eafd83ff529
r-santtos <rsanttos.dev@gmail.com>
2022-03-16 12:53:30 -0300

    new app

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -7,10 +7,6 @@ import {
   StatusBar,
   Platform
 } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import FinClasses from "../../components/FinClasses";
-import FinDocs from "../../components/FinDocs";
-import FinBooks from "../../components/FinBooks";
 
 type StackDashboard = {navigation: {navigate:Function}}
 
@@ -27,67 +23,8 @@ const Dashboard: React.FC<StackDashboard> = ({navigation}) => {
         justifyContent: 'flex-start',
       }}
     >
-      {/** COMPONENT FINCLASSES */}
-      <View style={styles.card}>
-        <View style={styles.header}>
-          <View style={styles.containerLeft}>
-            <View style={[styles.icon, {borderRadius: 25/2}]} />
-            <Text style={[styles.txt, styles.title]}>
-              Fin<Text style={{color: '#43e4de'}}> classes</Text>
-            </Text>
-          </View>
-
-          <View>
-            <View style={styles.links}>
-              <Text style={[styles.txt, {marginRight: 8}]}>ver todos</Text>
-              <MaterialIcons name="arrow-forward-ios" size={11} color="#fff" />
-            </View>
-          </View>
-        </View>
-
-        <FinClasses />
-      </View>
-
-      {/** COMPONENT FIN DOCS */}
-      <View style={styles.card}>
-        <View style={styles.header}>
-          <View style={styles.containerLeft}>
-            <View style={[styles.icon, {borderRadius: 25/2}]} />
-            <Text style={[styles.txt, styles.title]}>
-              Fin<Text style={{color: '#43e4de'}}> docs</Text>
-            </Text>
-          </View>
-
-          <View>
-            <View style={styles.links}>
-              <Text style={[styles.txt, {marginRight: 8}]}>ver todos</Text>
-              <MaterialIcons name="arrow-forward-ios" size={11} color="#fff" />
-            </View>
-          </View>
-        </View>
-        
-        <FinDocs />
-      </View>
-
-      {/** COMPONENT FIN BOOKS */}
-      <View style={styles.card}>
-        <View style={styles.header}>
-          <View style={styles.containerLeft}>
-            <View style={[styles.icon, {borderRadius: 8}]} />
-            <Text style={[styles.txt, styles.title]}>
-              Fin<Text style={{color: '#43e4de'}}> books</Text>
-            </Text>
-          </View>
-
-          <View>
-            <View style={styles.links}>
-              <Text style={[styles.txt, {marginRight: 8}]}>ver todos</Text>
-              <MaterialIcons name="arrow-forward-ios" size={11} color="#fff" />
-            </View>
-          </View>
-        </View>
-        
-        <FinBooks />
+      <View>
+        <Text style={styles.txt}>Dashboard</Text>
       </View>
     </ScrollView>
   )
@@ -103,47 +40,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     color: '#fff'
   },
-
-  /** CARD */
-  card: {
-    flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-  },
-
-  /** HEADER */
-  header: {
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 16,
-    paddingVertical: 16,
-  },
-  containerLeft: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start'
-  },
-  icon: {
-    width: 25,
-    height: 25,
-    borderWidth: 2,
-    borderColor: '#43e4de'
-  },
-  title: {
-    borderRadius: 8,
-    paddingHorizontal: 4,
-    paddingVertical: 4,
-    backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    marginLeft: -8,
-  },
-  links: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 4,
-  }
 })
 
 export default Dashboard;
